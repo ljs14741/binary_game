f1ceb69571d1c4edd241cf588a7fd11f4c03a0a0
binary <ljs14741@naver.com>
2025-07-05 11:28:42 +0900

    헤더 푸터 css 등 공통 변경사항 적용

--- src/main/java/com/example/game/controller/MainController.java
+++ src/main/java/com/example/game/controller/MainController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -44,5 +45,20 @@ public class MainController {
 
         model.addAttribute("user", user);
         return "main";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "common/about";
+    }
+
+    @GetMapping("/privacy-policy")
+    public String privacyPolicy() {
+        return "common/privacy-policy";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "common/contact";
     }
 }
